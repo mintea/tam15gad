c32fe49ac6bc461b3aaa9255650f49286879777e
Davee Nguyen <vibrantdavee@gmail.com>
2013-03-27 15:52:04 -0700

    Updated Player class to use weapon

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 using System.Collections;
 
 public class Player : Unit {
+	public Weapon weapon;
 	
 	// Use this for initialization
 	void Start () {
 		rotSpeed = 100;
 		_transform = transform;
+		weapon = _transform.GetComponentInChildren<Weapon>();
+		
 	}
 	void Awake() {
 		
@@ -23,5 +26,25 @@ public class Player : Unit {
 			RotateUnit();
 			MoveUnit();
 		}
+		
+		if (Input.GetKeyDown(KeyCode.JoystickButton11)){
+			ChangeWeapon();
+		}
+		
+		if (weapon == null) {
+			weapon = _transform.GetComponentInChildren<Weapon>();
+		}
+		else {
+			weapon.SetDirection(Input.GetAxis ("HorizontalFire"),Input.GetAxis ("VerticalFire"));
+			// shoot if using joystick
+			if (weapon.direction != Vector3.zero) {
+				weapon.Shoot((WeaponName)curWeapon);
+			}
+		}
+		
+	}
+	
+	void ChangeWeapon() {
+		curWeapon = (curWeapon+1) % 3;
 	}
 }
